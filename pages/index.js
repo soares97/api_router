f5f1e1cd3e06f166ad01c9a08a8e816f34f181ca
soares97 <michel971345g@gmail.com>
2021-05-31 11:22:18 +0000

    API pré terminado

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,16 @@
 export default function Home(props) {
-    console.log('Passando pelo Front-End')
-    const tempoDinamico = new Date();
-    const tempoD = tempoDinamico.toGMTString();
-    /*
-        return (
-            <div>
-                <p>{props.alunos.id}</p>
-                <p>{props.alunos.title}</p>
-            </div>
-        )
-    */
-
-/*
-    {
-        props.alunos.map(poste => {
-            return (
-                <div>
-                    <h3>{poste.id}</h3>
-                    <p>{poste.nome}</p>
-                    <p>{poste.idade}</p>
-                </div>
-            )
-        })
-    }
-*/
-    
      while (props.alunos) {
          return (
             <div>
-                <h1>Página Home - dessa vez eu consegui bby...kkkk</h1>
-                <p>{tempoD} (Dinamico)</p>
-                <p>{props.alunos.id}</p>
-                <p>{props.alunos.nome}</p>
-                <p>{props.alunos.idade}</p>
+                <h1>Rota de API</h1>
+                <p>http://localhost:3000/api/informaticos</p>
             </div>
          )
      }
- 
-
 }
 
 export async function getStaticProps() {
-    //console.log('Passando pelo Back-End')
-    //const tempoEstatico = new Date();
-    //const tempoE = tempoEstatico.toGMTString();
-
-    const response = await fetch('http://localhost:3000/api/informaticos/?id=4');
+    const response = await fetch('http://localhost:3000/api/informaticos/?id=1');
     const alunos = await response.json();
 
     return {
