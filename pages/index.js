6091138e710bb7d74411356b5a4e4aa204eceff5
soares97 <michel971345g@gmail.com>
2021-05-31 10:19:00 +0000

    teste

--- pages/index.js
+++ pages/index.js
@@ -2,28 +2,29 @@ export default function Home(props) {
     console.log('Passando pelo Front-End')
     const tempoDinamico = new Date();
     const tempoD = tempoDinamico.toGMTString();
+    /*
+        return (
+            <div>
+                <p>{props.alunos.id}</p>
+                <p>{props.alunos.title}</p>
+            </div>
+        )
+    */
+
 /*
-    return (
-        <div>
-            <p>{props.alunos.id}</p>
-            <p>{props.alunos.title}</p>
-        </div>
-    )
+    {
+        props.alunos.map(poste => {
+            return (
+                <div>
+                    <h3>{poste.id}</h3>
+                    <p>{poste.nome}</p>
+                    <p>{poste.idade}</p>
+                </div>
+            )
+        })
+    }
 */
     
-    {
-                props.alunos.map(poste => {
-                    return (
-                        <div>
-                            <h3>{poste.id}</h3>
-                            <p>{poste.nome}</p>
-                            <p>{poste.idade}</p>
-                        </div>
-                    )
-                })
-            }
-    
-    /*
      while (props.alunos) {
          return (
             <div>
@@ -34,9 +35,8 @@ export default function Home(props) {
                 <p>{props.alunos.idade}</p>
             </div>
          )
-
      }
- */
+ 
 
 }
 
@@ -45,7 +45,7 @@ export async function getStaticProps() {
     //const tempoEstatico = new Date();
     //const tempoE = tempoEstatico.toGMTString();
 
-    const response = await fetch('https://github.com/soares97/api_router/blob/main/pages/api/informaticos');
+    const response = await fetch('http://localhost:3000/api/informaticos/?id=4');
     const alunos = await response.json();
 
     return {
