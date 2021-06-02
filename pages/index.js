dc72bdbadadc6f836078b632898f5df06a241dda
soares97 <michel971345g@gmail.com>
2021-06-02 11:54:28 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-export default function Home(props) {
-     while (props.alunos) {
+export default function Home() {
          return (
             <div>
                 <h1>Rota de API</h1>
                 <p>http://localhost:3000/api/informaticos</p>
             </div>
          )
-     }
 }
