777f9a5c39ddaed5a7befd9bb3698ae3a8632784
soares97 <michel971345g@gmail.com>
2021-06-03 10:32:16 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
+import Link from 'next/link'
+
 export default function Home() {
-         return (
-            <div>
-                <h1>Rota de API</h1>
-                <p>http://localhost:3000/api/informaticos</p>
-            </div>
-         )
+    return (
+        <div className = "card">
+            <h1>Rota de API</h1>
+            <Link href="https://api-router-lvwfbi6si-soares97.vercel.app/api/informaticos">
+                <a>Endpoint para conjuntos de Informáticos</a>
+            </Link>
+            <style jsx>
+                {`
+                    a{
+                        padding: 0 10px;
+                        text-decoration:none;
+                        color: rgb(0,0,0);
+                        background-color:#dddddd;
+                        padding: 5px;
+	                    --margin: 2px;
+                    }
+                    
+                    .card {
+                        width: 20%;
+                        margin: 20px auto;
+                        text-align: center;
+                        padding: 1.5rem;
+                        font-size: 16pt;
+                        text-decoration: none;
+                        border: 2px solid #eaeaea;
+                        border-radius: 10px;
+                    }
+                `}
+            </style>
+        </div>
+    )
 }
